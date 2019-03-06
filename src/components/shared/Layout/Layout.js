d57e250407259c2f6bd126a76a329113877b4362
Kranti Nebhwani <darkphoton@live.com>
2019-03-06 19:40:57 +0800

    Minor tweaks and comments.

--- src/components/shared/Layout/Layout.js
+++ src/components/shared/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Header';
 
 const Layout = ({ children }) => {
+  // { children } // same as const children = props.children;, ES6 destructuring
   return (
     <div className="container">
       <Header />
